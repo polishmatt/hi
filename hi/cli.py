4cb1c9ba2f7199234234fa893360199a7614687a
Matt Wisniewski <hello@mattw.us>
2017-06-14 00:15:54 -0400

    add option for configurable hosts/groups files

--- hi/cli.py
+++ hi/cli.py
@@ -15,9 +15,18 @@ except (ValueError, SystemError):
 @click.argument('argv', nargs=-1)
 @click.version_option(version=version)
 @click.option('--run/--no-run', default=True)
+@click.option('--hosts-file')
+@click.option('--groups-file')
 def cli(**kwargs):
-    kwargs['hosts'] = hi.load_hosts()
-    kwargs['groups'] = hi.load_groups()
+    kwargs['hosts'] = hi.load_hosts(kwargs['hosts_file'])
+    # Don't use the default groups file when using a custom hosts file to avoid unintended behavior/confusion
+    if kwargs['hosts_file'] is not None and kwargs['groups_file'] is None:
+        kwargs['groups'] = {}
+    else:
+        kwargs['groups'] = hi.load_groups(kwargs['groups_file'])
+    del kwargs['hosts_file']
+    del kwargs['groups_file']
+
     sys.exit(hi.run(**kwargs))
 
 if __name__ == '__main__':
