b4fab6efb3a33fe23d46197785fea9687fd782ab
Matt Wisniewski <hello@mattw.us>
2017-06-14 00:16:16 -0400

    add and use fixtures for cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ class CLITest(HiTest):
     def test_exit_default(self):
         with open(os.devnull, 'w') as FNULL:
             subprocess.check_call(
-                ['python', '-mhi'],
+                ['python', '-mhi', '--hosts-file', 'tests/fixtures/hosts'],
                 stdout=FNULL,
                 stderr=subprocess.STDOUT
             )
@@ -16,7 +16,7 @@ class CLITest(HiTest):
     def test_exit_norun(self):
         with open(os.devnull, 'w') as FNULL:
             subprocess.check_call(
-                ['python', '-mhi', '--no-run', 'exit_norun'],
+                ['python', '-mhi', '--hosts-file', 'tests/fixtures/hosts', '--no-run', 'exit_norun'],
                 stdout=FNULL,
                 stderr=subprocess.STDOUT
             )
@@ -24,7 +24,7 @@ class CLITest(HiTest):
     def test_exit_success(self):
         with open(os.devnull, 'w') as FNULL:
             subprocess.check_call(
-                ['python', '-mhi', 'exit_success'],
+                ['python', '-mhi', '--hosts-file', 'tests/fixtures/hosts', 'exit_success'],
                 stdout=FNULL,
                 stderr=subprocess.STDOUT
             )
@@ -33,7 +33,7 @@ class CLITest(HiTest):
         try:
             with open(os.devnull, 'w') as FNULL:
                 subprocess.check_call(
-                    ['python', '-mhi', 'exit_failure'],
+                    ['python', '-mhi', '--hosts-file', 'tests/fixtures/hosts', 'exit_failure'],
                     stdout=FNULL,
                     stderr=subprocess.STDOUT
                 )
