24f5ed35e54c2f3909f9b096349d7e94f1acb250
Matt Wisniewski <hello@mattw.us>
2017-06-16 00:41:43 -0400

    start filling out real setup

--- setup.py
+++ setup.py
@@ -1,5 +1,46 @@
-req
-click
+from setuptools import setup
+import importlib
 
-test req
-mock
+version = importlib.import_module('hi.config').version
+
+setup(
+    name='hi',
+    version=version,
+    description='',
+    long_description="",
+    author='Matt Wisniewski',
+    author_email='hicli@mattw.us',
+    license='MIT',
+    url='https://github.com/polishmatt/hi',
+    keywords=[''],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    platforms=['unix','linux'],
+    packages=[
+        'hi'
+    ],
+    install_requires=[
+        'click==6.7',
+        'pyyaml==3.12'
+    ],
+    entry_points={
+        'console_scripts': [
+            'hi = hi.cli:cli'
+        ],
+    },
+)
