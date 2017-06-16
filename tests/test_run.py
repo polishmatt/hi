8ee661f0e91e33a6309eaecc350c572269bafeb7
Matt Wisniewski <hello@mattw.us>
2017-06-16 00:13:04 -0400

    tests for basic use cases

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,11 +1,11 @@
 #from unittest.mock import patch
-from mock import patch
+from mock import patch, call
 from tests import HiTest
 
 class RunTest(HiTest):
 
     @patch('hi.hi.log')
-    def assert_run(self, argv, command, mock_log):
+    def assert_run(self, argv, output, mock_log):
         if not hasattr(argv, '__iter__'):
             argv = [argv]
 
@@ -16,8 +16,24 @@ class RunTest(HiTest):
             'argv': argv,
         }
         self.hi.run(**kwargs)
-        mock_log.assert_called_once_with(command)
+
+        if hasattr(output, '__iter__'):
+            mock_log.assert_has_calls([call(line) for line in output])
+        else:
+            mock_log.assert_called_once_with(output)
 
     def test_command(self):
-        self.assert_run('command', 'ssh command')
+        self.assert_run('command', 'start command')
+
+    def test_group(self):
+        self.assert_run('group', 'start group end')
+
+    def test_args(self):
+        self.assert_run('args', 'start args end')
+
+    def test_exact(self):
+        self.assert_run('exact', 'start exact')
+
+    def test_multiple(self):
+        self.assert_run('multiple', ['multiple1', 'multiple2'])
 
