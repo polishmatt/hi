2c3475c3c989b6e5ce78824d68ca3265d758d4e2
Matt Wisniewski <hello@mattw.life>
2022-06-09 01:29:13 -0400

    simplify import test

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,13 +3,9 @@ from tests import HiTest
 
 class ImportTest(HiTest):
     def test_import(self):
-        expected = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'hicli'))
-        expected = [
-            os.path.join(expected, 'hi.py'),
-            os.path.join(expected, '__init__.py')
-        ]
+        expected = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'hicli', '__init__.py'))
         found = self.hi.__file__
-        if found not in expected:
+        if found != expected:
             self.fail("""
                 Version of hi imported is not the version we want to test
                 Found: '%s'
