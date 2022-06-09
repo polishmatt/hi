28ac846e9b9623a0ffb50858e17ec504c1f6976e
Matt Wisniewski <hello@mattw.life>
2022-06-09 00:37:18 -0400

    completely remove imp

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,8 +5,7 @@ import os
 class HiTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(HiTest, self).__init__(*args, **kwargs)
-        module = imp.find_module('hi')
-        self.hi = importlib.import_module('hi'))
+        self.hi = importlib.import_module('hi')
 
         fixtures = os.path.join(os.path.dirname(__file__), 'fixtures')
         self.hosts_file = os.path.join(fixtures, 'hosts')
